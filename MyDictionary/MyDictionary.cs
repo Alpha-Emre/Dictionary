434a5f931e05626544c586f19c96a82da9a5f260
Emre Kara <emresamsun551@outlook.com>
2021-03-08 20:45:44 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Id, Customer>
+    {
+        Id[] Keyarray;
+        Customer[] Valuearray;
+        public MyDictionary()
+        {
+            Keyarray = new Id[0];
+            Valuearray = new Customer[0];
+        }
+        public void Add(Id ıd, Customer customer)
+        {
+            Id[] TempKeys = new Id[Keyarray.Length];
+            Customer[] TempValue = new Customer[Valuearray.Length];
+
+            TempKeys = Keyarray;
+            TempValue = Valuearray;
+
+            Keyarray = new Id[Keyarray.Length + 1];
+            Valuearray = new Customer[Valuearray.Length + 1];
+
+            for (int i = 0; i < TempKeys.Length; i++)
+            {
+                Keyarray[i] = TempKeys[i];
+                Valuearray[i] = TempValue[i];
+
+            }
+            Keyarray[Keyarray.Length - 1] = ıd;
+            Valuearray[Valuearray.Length - 1] = customer;
+        }
+        public void List()
+        {
+            for (int i = 0; i < Keyarray.Length; i++)
+            {
+                Console.WriteLine("ID:" + Keyarray[i] + " Customer: " + Valuearray[i]);
+
+            }
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Id_customer = new MyDictionary<int, string>();
+            Id_customer.Add(1, "Emre");
+            Id_customer.Add(2, "Yunus");
+            Id_customer.Add(3, "Çağrı");
+            Id_customer.Add(4, "Yeliz");
+            Id_customer.List();
+        }
+    }
+}
